f8c8348a1ddab281388a9d86a4bef38673f89b12
alyssasamp <134331900+alyssasamp@users.noreply.github.com>
2023-08-03 11:00:40 -0400

    holding data attributes

---
+++ employee.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, name, ID, department, job):
+        self.name= name 
+        self.ID= ID
+        self.department= department
+        self.job= job
+        

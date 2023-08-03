d73bfe55df3a426dedef1c83256733a6e9df352d
alyssasamp <134331900+alyssasamp@users.noreply.github.com>
2023-08-03 10:42:06 -0400

    beginning of pet problem

---
+++ pet.py
@@ -0,0 +1,23 @@
+class Pet:
+    def __init__(self, name, animal_type, age):
+        self.name= name 
+        self.animal_type= animal_type
+        self.age= age 
+        
+    def set_name(self):
+        return self.name 
+    
+    def set_animal_type(self):
+        return self.animal_type
+    
+    def set_age(self):
+        return self.age 
+    
+    def get_name(self):
+        return self.name 
+    
+    def get_animal_type(self):
+        return self.animal_type
+    
+    def get_age(self):
+        return self.age 
